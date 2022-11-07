eb62a9a11007bbd6f04a874aacde286516955dcf
Eduardo Prieto <eduardo.prieto@nexgen-soluciones.com>
2022-11-07 12:21:50 -0500

    changes to contact add, update

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-edit',
   templateUrl: './contact-edit.component.html',
-  styleUrls: ['./contact-edit.component.css']
+  styleUrls: ['./contact-edit.component.css'],
 })
 export class ContactEditComponent implements OnInit {
+  originalContact: Contact = {};
+  contact: Contact = {};
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string = '';
 
-  constructor() { }
+  constructor(
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      const { id } = params;
+
+      if (id == undefined || id == null) {
+        this.editMode = false;
+        return;
+      }
+
+      this.originalContact = this.contactService.getContact(id);
+      if (this.originalContact == undefined || this.originalContact == null) {
+        return;
+      }
+      this.editMode = true;
+      this.contact = JSON.parse(JSON.stringify(this.originalContact));
+
+      if (this.contact.group != undefined && this.contact.group.length > 0) {
+        this.groupContacts = this.contact.group.slice();
+      }
+    });
   }
 
+  onSubmit(form: NgForm) {}
+
+  onRemoveItem(idx: number) {}
+
+  onCancel() {
+    this.router.navigateByUrl('/contacts');
+  }
 }
